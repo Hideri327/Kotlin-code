bc93357a8c1f41949fcc8fca3baf1827023227ee
Hideri327 <69734666+Hideri327@users.noreply.github.com>
2021-11-21 23:27:19 -0500

    Create Kotlin_pract.kt

---
+++ Kotlin_pract.kt
@@ -0,0 +1,18 @@
+fun main() {
+   
+    printBorder()
+    println("Happy Birthday, Jhansi!")
+    printBorder()
+    
+   }
+    
+    fun printBorder() {
+    repeat(23){
+        print("=")
+    } 
+      println()
+   }
+    
+   
+    
+    
